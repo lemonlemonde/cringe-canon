826e8de1fa20278a191a39e850f2ea769ba686f8
lemonlemonde <mirudjun@gmail.com>
2025-03-14 22:51:02 -0700

    :sparkles: Nextjs backend --> Flask backend

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # cringe-canon
 takes your cringe, makes it canon
 
-## Quick start
+## Quick start (frontend)
 ```bash
 cd cringe-canon
 npm install
 npm run dev
+```
+
+
+## Quick start (backend)
+```bash
+cd flask-backend
+conda create -n cringe-canon python=3.10.15
+conda activate cringe-canon
+pip install flask
+
+python app.py
 ```
---
+++ cringe-canon/src/app/api/upload/route.js
@@ -0,0 +1,37 @@
+import { writeFile } from "fs/promises";
+import path from "path";
+
+// get file that was POSTed to /api/upload
+  // from page.js
+export async function POST(request) {
+  const formData = await request.formData();
+  const file = formData.get("file");
+
+  if (!file) {
+    return Response.json({ message: "No file received" }, { status: 400 });
+  }
+
+  // const bytes = await file.arrayBuffer();
+  // const buffer = Buffer.from(bytes);
+
+  // Save to /public/uploads directory 
+  // const filename = file.name;
+  // const filePath = path.join(process.cwd(), "public", "uploads", filename);
+  // console.log("YOYOYOYO")
+  // return Response.json({ message: "File uploaded!", filename });
+
+  // await writeFile(filePath, buffer);
+
+  // Send to Flask backend
+  const res = await fetch("http://localhost:8000/upload", {
+    method: "POST",
+    body: formData,
+  });
+
+  const flaskResponse = await res.json();
+
+  return Response.json({
+    message: "File uploaded and forwarded to Flask!",
+    flaskResponse,
+  });
+}
